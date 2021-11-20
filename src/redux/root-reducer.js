f19625070c35c513e16c0e63306b3ae82e83e386
Amin El-Bekry <amin@Amins-MacBook-Pro.local>
2021-11-20 09:36:17 +0100

    added cart dropddown

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 export default combineReducers({
     // set the user reducer using the key 'user'
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
