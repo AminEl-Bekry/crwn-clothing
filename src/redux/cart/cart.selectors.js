25a1e54292590a7feee6c029a072a4d0b3ce7a18
Amin El-Bekry <amin@Amins-MacBook-Pro.local>
2021-11-20 17:06:33 +0100

    updating our app with reselect library. replaced cart selectors with memoized selectprs

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,22 @@
+import { createSelector } from 'reselect';
+
+// takes the whole state and returns a slice of it, 1 layer deep.
+// this is an inout selector
+const selectCart = state => state.cart
+
+// creates a selector to select cart items which is now a memoized selector
+export const selectCartItems = createSelector(
+    // first argument this takes is an array or the select we want.
+    [selectCart],
+    // second argument is a function that will return the value we want from the selector
+    (cart) => cart.cartItems
+)
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedQuantity, cartItem) =>
+            accumulatedQuantity + cartItem.quantity,
+            0
+    )
+)
