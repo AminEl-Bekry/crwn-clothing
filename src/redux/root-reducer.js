32c8bb0b1fd30530f8561780299c277529bdf301
Amin El-Bekry <amin@Amins-MacBook-Pro.local>
2021-11-21 20:19:35 +0100

    moved directory and shop state into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,26 @@
 import { combineReducers } from "redux";
+import { persistReducer } from 'redux-persist';
+// selecting localStorage from redux-persist to use as default storage
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducers";
 
-export default combineReducers({
+// configuring the localStorage to persist the cart reducer to cache the cart state in the browser
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     // set the user reducer using the key 'user'
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer,
+    shop: shopReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -7,5 +8,7 @@ import rootReducer from './root-reducer';
 const middlewares = [logger]
 // creating our store and passing through the middleware
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
+// caches the store in the browser using localStorage to keep the state of cart on reload.
+const persistor = persistStore(store)
 
-export default store
+export { store, persistor }
