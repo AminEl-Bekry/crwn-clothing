871cca738e019996e676c5fa81ba11b75c817156
Amin El-Bekry <amin@Amins-MacBook-Pro.local>
2021-11-28 17:07:17 +0100

    added stripe integration, favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+import './stripe-button.styles.scss'
+
+const StripeCheckoutButton = ({ price }) => {
+    // Stripe wants all prices in cents so $50 = 5000 so we do this calculation to make code readable
+    const priceForStripe = price * 100
+    const publishableKey = 'pk_test_51K0oueHecTnGvU3DV7H9gfzIKCmmTqhMNnANgV2vLcbW8ekseuz4OnvDDmVSxfX56EKO0YwkMMc3HS1bgKBPOO2Q00kAyfy68A'
+    const onToken = token => {
+        console.log(token)
+        alert('Payment successful!')
+    }
+
+    return(
+        <StripeCheckout
+            label='Pay now'
+            name='Crwn Clothing AB'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton
